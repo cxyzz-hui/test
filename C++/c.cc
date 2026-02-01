79ac8a52331c14f957381479aa444744f7d4ef18
cxyzz-hui <1929697949@qq.com>
2026-02-01 19:15:06 +0800

    test

---
+++ C++/c.cc
@@ -0,0 +1,15 @@
+#define C std::cout <<
+#define CC std::string test = "hello , world";
+#define CCC << std::endl;
+#define CCCC test
+#define cc return 0;
+
+#include <iostream>
+
+int main(void)
+{
+    CC 
+    C CCCC CCC 
+
+    cc
+}
